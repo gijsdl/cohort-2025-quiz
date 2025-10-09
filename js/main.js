248c88dee9dedf69f97fb90bca14b9b977cd26ee
Gijs de Lange <g.de.lange@rocmondriaan.nl>
2025-10-09 15:51:46 +0200

    added questions

---
+++ js/main.js
@@ -0,0 +1,125 @@
+console.log("main loaded");
+
+
+
+const questions = [
+    "Waarvoor gebruik je HTML?",
+    "Wat betekent de afkorting HTML?",
+    "Welk HTML-element gebruik je voor een koptekst?",
+    "Welk element gebruik je om een paragraaf te maken?",
+    "Wat doet het <img> element?",
+    "Waarvoor gebruik je CSS?",
+    "Wat betekent CSS?",
+    "Welke eigenschap gebruik je om tekstkleur te veranderen in CSS?",
+    "Hoe geef je een achtergrondkleur in CSS?",
+    "Hoe verbind je een CSS-bestand met HTML?",
+    "Waarvoor gebruik je JavaScript?",
+    "Wat betekent alert(\"Hallo!\") in JavaScript?",
+    "Wat is een variabele in JavaScript?",
+    "Welke van deze is een geldige variabele?",
+    "Wat doet console.log()?",
+    "Wat is een browser?",
+    "Welke van deze is GEEN browser?",
+    "Wat is een hyperlink?",
+    "Wat doet <a href=\"...\"> in HTML?",
+    "Wat is het verschil tussen HTML en CSS?"
+];
+
+const answerA = [
+    "Om foto's te bewerken",
+    "HyperText Markup Language",
+    "<p>",
+    "<p>",
+    "Maakt een tabel",
+    "Om tekst te schrijven",
+    "Computer Style Sheet",
+    "font-size",
+    "text-color",
+    "<style>",
+    "Om websites sneller te maken",
+    "Verandert de achtergrondkleur",
+    "Een foutmelding",
+    "var naam = \"Jan\";",
+    "Stuurt een bericht naar een andere website",
+    "Een programma om websites te bekijken",
+    "Chrome",
+    "Een soort afbeelding",
+    "Voegt een afbeelding toe",
+    "HTML is voor opmaak, CSS voor structuur"
+];
+
+const answerB = [
+    "Om websites te maken",
+    "High Tech Machine Language",
+    "<h1>",
+    "<h2>",
+    "Voegt een afbeelding toe",
+    "Om de stijl van een website te bepalen",
+    "Creative Style System",
+    "text-align",
+    "bg-color",
+    "<script>",
+    "Om interactieve dingen te doen op een website",
+    "Stuurt een e-mail",
+    "Een stukje data dat je kunt opslaan",
+    "variable naam = Jan;",
+    "Laat iets zien in de browserconsole",
+    "Een soort computer",
+    "Firefox",
+    "Een link naar een andere pagina",
+    "Maakt een koptekst",
+    "HTML is voor structuur, CSS voor opmaak"
+];
+
+const answerC = [
+    "Om muziek af te spelen",
+    "Home Tool Markup Language",
+    "<div>",
+    "<img>",
+    "Maakt een link",
+    "Om bestanden op te slaan",
+    "Cascading Style Sheets",
+    "color",
+    "background-color",
+    "<link>",
+    "Om afbeeldingen te bewerken",
+    "Laat een pop-up zien met \"Hallo!\"",
+    "Een afbeelding",
+    "naam = var \"Jan\";",
+    "Verandert de kleur van tekst",
+    "Een programmeertaal",
+    "Safari",
+    "Een koptekst",
+    "Maakt een link",
+    "HTML is een programmeertaal"
+];
+
+const answerD = [
+    "Om e-mails te versturen",
+    "Hyperlink Text Main Language",
+    "<link>",
+    "<ul>",
+    "Voegt tekst toe",
+    "Om video's af te spelen",
+    "Code Styling Software",
+    "background",
+    "color-background",
+    "<css>",
+    "Om tekst op te maken",
+    "Verwijdert tekst",
+    "Een link naar een website",
+    "Jan = naam;",
+    "Maakt een nieuwe pagina",
+    "Een tekstverwerker",
+    "Excel",
+    "Een achtergrondkleur",
+    "Verandert de kleur",
+    "CSS maakt websites zichtbaar"
+];
+
+const correctAnswer = [
+    "B", "A", "B", "A", "B",
+    "B", "C", "C", "C", "C",
+    "B", "C", "B", "A", "B",
+    "A", "D", "B", "C", "B"
+];
